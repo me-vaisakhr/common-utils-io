60355afb73ac2ea395bc6c84f5824d9612613f49
vaisakh.krishnan <vaisakhr.me@gmail.com>
2024-01-18 14:50:15 +0530

    Update the test description

--- test/strings/transform.test.ts
+++ test/strings/transform.test.ts
@@ -1,9 +1,15 @@
 import { capitalizeFirstLetter } from "../../src/strings/transform";
 
 describe("string/transform", () => {
-	describe("Getting percentage from value", () => {
-		it("should give capitalized correctly", () => {
+	describe("Capitalize a string", () => {
+		it("should give capitalized correctly with lowercase letters", () => {
 			expect(capitalizeFirstLetter("sample")).toEqual("Sample");
+		});
+		it("should give capitalized correctly with uppercase letters", () => {
+			expect(capitalizeFirstLetter("SAMPLE")).toEqual("Sample");
+		});
+		it("should give capitalized correctly with random case letters", () => {
+			expect(capitalizeFirstLetter("SaMpLe")).toEqual("Sample");
 		});
 	});
 });
